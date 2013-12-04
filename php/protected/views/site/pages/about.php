a6d703dce7884a2c6a3a8c0e6314fb641ad61e72
Md. Rashidul Hasan <masums@gmail.com>
2013-12-04 21:02:55 +0600

    about, main and index.php files are changed

--- php/protected/views/site/pages/about.php
+++ php/protected/views/site/pages/about.php
@@ -9,4 +9,4 @@ $this->breadcrumbs=array(
 <h1>About</h1>
 
 <p>This is a "static" page. You may change the content of this page
-by updating the file <code><?php echo __FILE__; ?></code>.</p>
+by updating the file <code>/protected/views/site/pages/about.php</code>.</p>
--- php/themes/bootstrap/views/site/index.php
+++ php/themes/bootstrap/views/site/index.php
@@ -18,11 +18,26 @@ $this->pageTitle=Yii::app()->name;
 <p>You may change the content of this page by modifying the following two files:</p>
 
 <ul>
-    <li>View file: <code><?php echo __FILE__; ?></code></li>
-    <li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
+    <li>View file: <code> /themes/bootstrap/views/site/index.php </code></li>
+    <li>Layout file: <code>/themes/bootstrap/views/layouts/main.php</code></li>
+	<li>Database Access: 
+		<code>
+			Create database tables from OpenShift PhpMyAdmin panel.
+			<br/>From your main config file enable gii module and add your IP.
+			<br/>Then you will be able to start gii and connect with your database for CRUD operation
+		</code>
+	</li>
+	
 </ul>
 
 <p>For more details on how to further develop this application, please read
     the <a href="http://www.yiiframework.com/doc/">documentation</a>.
     Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
     should you have any questions.</p>
+<p>
+	Quick Start application is prepared by <a target="_blank" href="http://masums.com">Md. Rashidul Hasan Masum</a>
+	<br/> If you fetch any problem to install this application please feel free to email me at
+	<br/> masum@masums.com
+	<br/>
+	Thanks for using this application
+</p>
